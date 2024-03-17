fd38679aac3f2a85683af5c4d4d0d6364e79acb0
crocs-in-socks <gouriswag1@gmail.com>
2024-03-18 00:28:17 +0530

    Integrated image capturing

--- frontend/app-etite/src/components/ImageDisplay.js
+++ frontend/app-etite/src/components/ImageDisplay.js
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react'
 
 const ImageDisplay = ({imageData}) => {
     return(
         <div>
-            {imageData && <img src={imageData} />}
+            {imageData && <img src={imageData} alt='Captured Image'/>}
         </div>
-    );
-};
+    )
+}
+
+export default ImageDisplay
--- frontend/app-etite/src/pages/Home.js
+++ frontend/app-etite/src/pages/Home.js
@@ -1,27 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
+
+import CameraComponent from '../components/CameraComponent'
+import ImageDisplay from '../components/ImageDisplay'
 
 function Home() {
     const navigate = useNavigate()
     const handleLogout = async () => {
-        try{
+        try {
           const response = await axios.post('/logout')
-          if(response.status === 200)
-          {
-            
+          if(response.status === 200) {
             navigate('/login')
           }
-        }catch(e)
-        {
-          console.log(e)
         }
-      }
-  return (
-    <div>Home
+        catch(error) {
+          console.log(error)
+        }
+    }
 
-        <button onClick={handleLogout}>Logout</button>
-    </div>
+    const [capturedImage, setCapturedImage] = useState(null)
+    const handleCapture = (imageData) => {
+      setCapturedImage(imageData)
+    }
+
+    return (
+        <div>Home
+            <CameraComponent onCapture={handleCapture} />
+            <ImageDisplay imageData={capturedImage} />
+            <button onClick={handleLogout}>Logout</button>
+        </div>
   )
 }
 
